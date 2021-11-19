97b7c1422ae64a7b917e5203a8a1b5200a687efb
wendellima94 <wendel.e.s.lima@gmail.com>
2021-11-19 11:57:18 -0300

    second commit

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -6,16 +6,16 @@ const About = () => {
   return (
     <main className={styles.container}>
       <section className={styles.iconsContainer}>
-        <a target="_blank" href="https://www.facebook.com/Alpha-Net-Guaruja-104177842095377/?notif_id=1637247908648139&notif_t=page_fan&ref=notif">
+        <a target="_blank" rel="noreferrer" href="https://www.facebook.com/Alpha-Net-Guaruja-104177842095377/?notif_id=1637247908648139&notif_t=page_fan&ref=notif">
           <BsFacebook />
         </a>
-        <a target="_blank" href="https://www.instagram.com/alphanetguaruja/?fbclid=IwAR1_ud8iXPqbe83yLrxmSYBq265Ho2o6-BZwo7XRTuGclaKuUjw5Tn_deR8">
+        <a target="_blank" rel="noreferrer" href="https://www.instagram.com/alphanetguaruja/?fbclid=IwAR1_ud8iXPqbe83yLrxmSYBq265Ho2o6-BZwo7XRTuGclaKuUjw5Tn_deR8">
           <BsInstagram />
         </a>
-        <a href="/" target="_blank">
+        <a href="/" rel="noreferrer" target="_blank">
           <BsTelephoneOutboundFill />
         </a>
-        <a href="/" target="_blank">
+        <a href="/" rel="noreferrer" target="_blank">
           <BsWhatsapp />
         </a>
       </section>
