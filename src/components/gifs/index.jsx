d0ca7dbabb42c21b2a0ddcfd2f1845f3de4a09ef
wendellima94 <wendel.e.s.lima@gmail.com>
2021-11-25 14:53:35 -0300

    add to responsive image

--- src/components/gifs/index.jsx
+++ src/components/gifs/index.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import GifCarreira from '../../assets/gifAnimado.gif'
-import GifCarreiraTwo from '../../assets/cel-empresa.gif'
 import styles from '../../styles/Gif.module.css';
 
 
@@ -9,8 +7,6 @@ function Gif() {
 
   return (
     <div className={styles.container}>
-      {/* <img className={styles.AnimateGif} src={GifCarreira} alt="loading..." */}
-      {/* /> */}
     </div>
   )
 }
