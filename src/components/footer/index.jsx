6036c0e34cdf97ccb55cadb4db095c62df2b366d
wendellima94 <wendel.e.s.lima@gmail.com>
2022-01-18 15:35:11 -0300

    att icons

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import style from '../../styles/Footer.module.css';
-import { BsFacebook, BsInstagram, BsWhatsapp } from "react-icons/bs";
+
+import { BsFacebook } from "react-icons/bs";
+import { AiOutlineWhatsApp } from "react-icons/ai";
+import { GrInstagram } from "react-icons/gr";
 
 export default function Footer() {
   const iconSize = 40
 
   return (
     <footer className={style.container}>
-      Entre em contato conosco por: 
+      Entre em contato conosco por:
       <a href="http://api.whatsapp.com/send?1=pt_BR&phone=55013997294828" rel="noreferrer" target="_blank">
-        <span>Whatsapp </span><BsWhatsapp color='green' size={iconSize} />
+        <AiOutlineWhatsApp color='#34af23' size={iconSize} /><span> Whatsapp</span>
       </a>
       <a target="_blank" rel="noreferrer" href="https://www.facebook.com/Alpha-Net-Guaruja-104177842095377/?notif_id=1637247908648139&notif_t=page_fan&ref=notif">
-        <span>Facebook </span><BsFacebook color='blue' size={iconSize} />
+        <BsFacebook color='#3b5998' size={iconSize} /><span> Facebook</span>
       </a>
       <a target="_blank" rel="noreferrer" href="https://www.instagram.com/alphanetguaruja/?fbclid=IwAR1_ud8iXPqbe83yLrxmSYBq265Ho2o6-BZwo7XRTuGclaKuUjw5Tn_deR8">
-        <span>Instagram </span><BsInstagram color='gray' size={iconSize} />
+        <GrInstagram color='gray' size={iconSize} /><span> Instagram </span>
       </a>
     </footer>
   )
